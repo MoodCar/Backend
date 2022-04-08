d05f4e260f02b9556d2ff319b16c47c463cc8e26
canonn11 <myungkwanokk@gmail.com>
2022-04-08 23:35:04 +0900

    Jenkins push 테스트 #8

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => res.send('Jenkins push test Success'));
+app.get('/', (req, res) => res.send('Jenkins push test Success!'));
 app.listen(3000, () => console.log('Server Up and running at 3000'));
