5876e34a304d47d06de532c9adc7625dc997dfb9
canonn11 <myungkwanokk@gmail.com>
2022-04-09 01:52:29 +0900

    Jenkins 배포 테스트 #6

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => res.send('Jenkins deploy test #4'));
+app.get('/', (req, res) => res.send('Jenkins deploy test #6'));
 app.listen(3000, () => console.log('Server Up and running at 3000'));
