d04e3080666236393c72311a5c286be65f084dac
vmxm7890 <yjs9804@ajou.ac.kr>
2022-04-09 15:47:10 +0900

    jenkins 배포 테스ㅡ트 #8

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => res.send('Jenkins deploy test #7'));
+app.get('/', (req, res) => res.send('Jenkins deploy test #8'));
 app.listen(3000, () => console.log('Server Up and running at 3000'));
