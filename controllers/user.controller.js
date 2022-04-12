75f8e2a1e1b127ec2a5d254cec3433dc86eb764e
vmxm7890 <yjs9804@ajou.ac.kr>
2022-04-12 13:15:37 +0900

    사용자 아이디 조회,삭제,등록,수정

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,3 +13,56 @@ exports.findAll = (req,res) => {
         else res.send(data);
     });
 };
+
+// User 아이디 별 조회
+exports.findById = (req,res) => {
+    User.getById(req.params.id, (err,data) => {
+        if(err){
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users"
+            });
+        }
+        else res.send(data);
+    });
+};
+
+// User 아이디 별 삭제
+exports.delete = (req,res) => {
+    User.delete(req.params.id, (err,data) => {
+        if(err){
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users"
+            });
+        }
+        else res.send(data);
+    });
+};
+
+// User 등록
+exports.create = (req, res)=> {
+    const user = new User(req.body);
+    User.create(user, (err,data) => {
+        if(err){
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users"
+            });
+        }
+        else res.send(user);
+    });
+};
+
+// User 아이디 별 수정
+exports.updateById = (req,res) => {
+    User.updateById(req.params.id,new User(req.body),(err,data) => {
+        if(err){
+            res.status(500).send({
+                message:
+                err.message || "Some error occurred while retrieving users"
+            });
+        }
+        else res.send(data);
+    });
+};
