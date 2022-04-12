5475dc98b469c09284ca4ef5dbfd73a62b343955
canonn11 <myungkwanokk@gmail.com>
2022-04-12 15:01:44 +0900

    Readme 수정

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,10 @@ module.exports = app =>{
     const users = require("../controllers/user.controller.js");
 
     app.get("/users",users.findAll);
-    app.get("/users/:id",users.findById);
+    //app.get("/users/:id",users.findById);
     app.delete("/users/:id",users.delete);
-    app.post("/users",users.create);
+    
+    //app.post("/users",users.create);
+
     app.patch("/users/:id",users.updateById);
 };
