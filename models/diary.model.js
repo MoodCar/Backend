e510734dc5a42e14fc6c0a531e7048ab9c3b388f
canonn11 <myungkwanokk@gmail.com>
2022-04-17 14:51:32 +0900

    Router 수정

--- models/diary.model.js
+++
@@ -1,8 +0,0 @@
-const sql = require('./db.js');
-
-const Diary = function(diary){
-    this.user_id = diary.user_id;
-    this.diary_title = diary.diary_title;
-    this.diary_content = diary.diary_content;
-}
-
--- models/user.model.js
+++ models/user.model.js
@@ -20,58 +20,5 @@ User.getAll = (result) =>{
     });
 };
 
-// User 아이디 별 조회
-User.getById = (id, result)=>{
-    sql.query("Select * from user where id = ? ", id, (err, res)=>{
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      
-      console.log("user:",res);
-      result(null, res);
-    });
-};
-
-// User 아이디 별 삭제
-User.delete = (id, result)=>{
-    sql.query("DELETE FROM user WHERE id = ?", id, (err, res)=>{
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      
-      console.log("user:",res);
-      result(null, res);
-    });
-};
-
-// User 등록
-User.create = (user, result)=>{
-    sql.query("INSERT INTO user set ?", user,(err, res)=>{
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      } 
-    console.log("user:",res.insertId);
-    result(null, res.insertId);
-    });
-};
-
-// User 정보 수정
-User.updateById = (id,user, result)=>{
-    sql.query("UPDATE user set password = ?,location=? where id = ?",[user.password,user.location,id],(err, res)=>{
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      } 
-    console.log("user:",{id:id, ...user});
-    result(null, {id:id, ...user});
-    });
-};
 
 module.exports = User;
