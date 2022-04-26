df878771c5a0f860dd826a50b0c70cbdaea64be2
canonn11 <myungkwanokk@gmail.com>
2022-04-26 13:55:51 +0900

    일기 관련 API 수정 및 로그인 확인 API 추가

--- controllers/diary.controller.js
+++ controllers/diary.controller.js
@@ -1,54 +1,9 @@
 const {Diary,idValid,diaryIdValid,contentExist} = require("../models/diary.model.js");
 const sql = require("../models/db.js");
-// Diary 작성
-exports.write = (req, res)=> {
-    const diary = new Diary(req.body);
-    sql.query("select * from user where providerId = ?",req.params.providerId,(err2,res2)=>{
-        if(res2.length === 0){
-            res.json({
-                isSuccess: false,
-                code: 404,
-                message: "ID does not exist."
-            })
-        }else{
-            if(diary.content === ""){
-                res.json({
-                    isSuccess: false,
-                    code: 400,
-                    message: "Please write a diary content."
-                })
-            }else{
-                sql.query("select * from diary where providerId = ? and written_date = curdate()",
-                req.params.providerId,(err,res3)=>{
-                if(res3.length !== 0){
-                    res.json({
-                        isSuccess: false,
-                        code: 400,
-                        message: "You already wrote today's diary."
-                    })
-                }else{
-                     Diary.write(req.params.providerId, diary,(err,data) => {
-                        if(err){
-                            res.status(400).send({
-                                message:
-                                err.message || "Diary request error"
-                            });
-                        }
-                        else res.json({
-                            isSuccess: true,
-                            code: 200
-                            })
-                        });
-                    }
-                })
-            }
-        }
-    });
-  
-};
+
 
 // 일기 작성(수정본)
-exports.write2 = (req, res)=> {
+exports.write = (req, res)=> {
     const diary = new Diary(req.body);
     const idvalid = new idValid(req.body);
     idValid.validate(req.params.providerId,idvalid,(err,data)=>{
--- routes/google.routes.js
+++ routes/google.routes.js
@@ -1,3 +1,5 @@
+const res = require("express/lib/response");
+
 module.exports = (app) => {
   const passport = require("passport");
 
@@ -7,7 +9,7 @@ module.exports = (app) => {
     passport.authenticate("google", { scope: ["email", "profile"] })
   );
 
-  // google login 성공과 실패 리다이렉트
+  // google login 성공과 실패 리다이렉트 라우트 지정
   app.get(
     "/auth/google/callback",
       passport.authenticate("google", {
@@ -19,17 +21,30 @@ module.exports = (app) => {
       }
   );
 
+  // login 실패 시 로그인창으로 다시 redirect
   app.get("/auth/google/fail",(req,res) => {
     res.redirect("/auth/google");
   });
 
-  app.get("/auth/google/success",(req,res) => {
-    res.status(200).send(req.user);
-  });
 
   // logout
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/auth/google");
   });
+
+  // login check 
+  app.get("/checklogin", (req,res) => {
+
+    if(!req.user){
+      res.status(400).send({
+        isSuccess : false,
+        code : 400,
+        message : "Login Required."
+      })
+    }
+    else{
+      res.json(req.user);
+    }
+  })
 };
