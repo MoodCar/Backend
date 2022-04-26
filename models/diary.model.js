0501afe78e04242a5bb5f47255b9b259f53dbccd
vmxm7890 <yjs9804@ajou.ac.kr>
2022-04-26 13:25:04 +0900

    일기 작성 및 삭제 API

--- models/diary.model.js
+++ models/diary.model.js
@@ -5,6 +5,62 @@ const Diary = function(diary){
     this.content = diary.content;
 }
 
+const idValid = function(idvalid){
+  this.valid = idvalid.valid;
+}
+
+idValid.validate = (providerId,idvalid,result)=>{
+  sql.query("select * from user where providerId = ?",providerId,(err,res)=>{
+    if(res.length === 0){
+      idvalid.valid = 0;
+      result(null,idvalid.valid);
+    }else{
+      idvalid.valid = 1;
+      result(null,idvalid.valid);
+    }
+  });
+};
+
+const diaryIdValid = function(diaryidvalid){
+  this.valid = diaryidvalid.valid;
+}
+
+diaryIdValid.validate = (id,diaryidvalid,result)=>{
+  sql.query("select * from diary where id = ?",id,(err,res)=>{
+    if(res.length === 0){
+      diaryidvalid.valid = 0;
+      result(null,diaryidvalid.valid);
+    }else{
+      diaryidvalid.valid = 1;
+      result(null,diaryidvalid.valid);
+    }
+  });
+};
+
+const contentExist = function(contentexist){
+  this.exist = contentexist.exist;
+}
+
+contentExist.isExist = (content,contentexist,result) =>{
+  if(content === ""){
+    contentexist.exist = 0;
+    result(null,contentexist.exist);
+  }else{
+    contentexist.exist = 1;
+    result(null,contentexist.exist);
+  }
+};
+
+Diary.alreadyExist = (providerId,result) =>{
+  sql.query("select * from diary where providerId = ? and written_date = curdate()",providerId,(err,res)=>{
+    if(res.length !== 0){
+      result(null,0);
+    }else{
+      result(null,1);
+    }
+  });
+};
+
 Diary.write = (providerId,diary,result)=>{
     sql.query("INSERT INTO diary(providerId,content) values (?,?)",
     [providerId,diary.content],(err, res)=>{
@@ -19,4 +75,38 @@ Diary.write = (providerId,diary,result)=>{
     });
 };
 
-module.exports = Diary;
+Diary.write2 = (providerId,diary,result)=>{
+  sql.query("INSERT INTO diary(providerId,content) values (?,?)",
+  [providerId,diary.content],(err, res)=>{
+  if(err){
+      res.json({
+        isSuccess: false,
+        code: 400,
+        message: "request to create diary is incorrect or corrupt"
+      });
+  }
+  //console.log("diary:",res.insertId);
+  result(null, res.insertId);
+  });
+};
+
+Diary.delete = (id, result)=>{
+  sql.query("DELETE FROM diary WHERE id = ?", id, (err, res)=>{
+    if(err){
+      res.json({
+        isSuccess: false,
+        code: 400,
+        message: "request to delete diary by diaryID is incorrect or corrupt"
+      });
+    }
+    //console.log("diary:",res);
+    result(null, res);
+  });
+};
+
+module.exports = {
+  Diary,
+  idValid,
+  diaryIdValid,
+  contentExist
+};
