26568c329b6f3bbee4e3458c077690d2333be448
canonn11 <myungkwanokk@gmail.com>
2022-04-30 19:33:53 +0900

    DB 연결방식 Single Connection Connection Pool로 변경

--- models/db.js
+++ models/db.js
@@ -2,74 +2,8 @@ const mysql = require("mysql");
 const dbConfig = require("../config/dbconfig.js");
 
 // 데이터베이스 connection 객체 생성
-const connection = mysql.createConnection(dbConfig);
+const connection = mysql.createPool(dbConfig);
 
 
-connection.connect(function(err){
-    if(err) {
-        // mysqlErrorHandling(connection, err);
-        console.log("\n\t *** Cannot establish a connection with the database. ***");
-
-        reconnect(connection);
-    }else {
-        console.log("\n\t *** New connection established with the database. ***")
-    }
-});
-
-//- Reconnection function
-function reconnect(connection){
-    console.log("\n New connection tentative...");
-
-    //- Destroy the current connection variable
-    if(connection) connection.destroy();
-
-    //- Create a new one
-    connection = mysql.createConnection(dbConfig);
-
-    //- Try to reconnect
-    connection.connect(function(err){
-        if(err) {
-            //- Try to connect every 2 seconds.
-            setTimeout(reconnect, 2000);
-        }else {
-            console.log("\n\t *** New connection established with the database. ***")
-            return connection;
-        }
-    });
-}
-
-//- Error listener
-connection.on('error', function(err) {
-
-    //- The server close the connection.
-    if(err.code === "PROTOCOL_CONNECTION_LOST"){    
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
-        reconnect(connection);
-    }
-
-    //- Connection in closing
-    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
-        reconnect(connection);
-    }
-
-    //- Fatal error : connection variable must be recreated
-    else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
-        reconnect(connection);
-    }
-
-    //- Error because a connection is already being established
-    else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
-    }
-
-    //- Anything else
-    else{
-        console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
-        reconnect(connection);
-    }
-
-});
 
 module.exports = connection;
