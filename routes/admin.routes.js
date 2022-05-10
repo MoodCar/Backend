df5b9a4e04a38f8fcdce57069588540750b411d7
vmxm7890 <yjs9804@ajou.ac.kr>
2022-05-10 16:59:30 +0900

    관리자 페이지(사용자/일기 - 전체 조회/삭제) 수정

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,16 +1,57 @@
+const axios = require('axios');
+
 module.exports = app =>{
-    const admin = require("../controllers/admin.controller.js");
 
-    app.get("/admin",admin.manage);
+    app.get("/admin", (req, res) => {
+        res.render('manage');
+    });
 
-    app.get("/admin/diaries",admin.diaries);
-    app.get("/admin/diaries/:providerId",admin.fetchUserByProviderId);
-    app.post("/admin/diaries/update/:id",admin.updateDiary);
-    app.get("/admin/diaries/updatePage/:id",admin.updateDiaryPage);
-    app.get("/admin/diaries/details/:id",admin.deleteDiary);
+    app.get("/admin/diaries", (req, res) => {
+        axios.get("http://localhost:3000/diaries")
+            .then(response => {
+                res.render('diaryList', {
+                    title: 'Diary List',
+                    diaries: response.data.getAllResult
+                })
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            })
+    });
 
-    app.get("/admin/users",admin.users);
-    app.post("/admin/users/update/:providerId",admin.updateUser);
-    app.get("/admin/users/updatePage/:providerId",admin.updateUserPage);
-    app.get("/admin/users/delete/:providerId",admin.deleteUser);
+    app.get("/admin/diaries/details/:id", (req, res) => {
+        axios.delete(`http://localhost:3000/diaries/details/${req.params.id}`)
+            .then(response => {
+                console.log(response.data.deleteResult);
+                res.redirect('/admin/diaries');
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            })
+    });
+
+    app.get("/admin/users", (req, res) => {
+        axios.get("http://localhost:3000/users")
+            .then(response => {
+                res.render('userList', {
+                    title: 'User List',
+                    users: response.data.userInfo
+                })
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            })
+    });
+
+    app.get("/admin/users/details/:providerId", (req, res) => {
+        axios.delete(`http://localhost:3000/users/${req.params.providerId}`)
+            .then(response => {
+                console.log(response.data.deleteResult);
+                res.redirect('/admin/users');
+            })
+            .catch(error => {
+                console.log('error: ', error);
+            })
+    });
+
 };
